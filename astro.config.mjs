a0bdfe6f17755e599370ac6c0f96c5c773471944
Grigory Griko <griko1996@gmail.com>
2023-10-18 15:31:57 +0300

    feat: деплой проекта для gh-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  //site: "https://grigorygriko.github.io",
-  base: "/src",
+  /*site: "https://grigorygriko.github.io",
+  base: "/astro_checklist",*/
   integrations: [react()]
 });
