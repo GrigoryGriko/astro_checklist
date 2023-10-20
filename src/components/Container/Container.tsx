694bfe25f4fb055896014950457c852f6497d09c
Grigory Griko <griko1996@gmail.com>
2023-10-20 16:25:37 +0300

    feat: make limiter list

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -5,18 +5,22 @@ import PointList from '../PointList/PointList';
 
 function Container() {
   const [list, setList] = useState([]);
-  
+  const [maxCountList, setMaxCountList] = useState(10);
 
+  function listLimiter(list) {
+    return list.slice().splice(0, maxCountList);
+  }
 
   useEffect(() => {
     const oldList = JSON.parse(localStorage.getItem("todoList"));
 
+
     if (oldList) {
-      setList(oldList);
+      setList(listLimiter(oldList));
     } else {
       api.getTodoList()
       .then((res) => {
-        setList(res);
+        setList(listLimiter(res));
         console.log(res);
       })
       .catch((err) => {
